09e10ce8d43b29b5771b1a4c02167047f21e5344
georgelofre <georgelofre@gmail.com>
2022-12-09 00:18:32 -0300

    criando a conexão com banco de dados local, pasta templates e arquivo html

--- relatorio/app.py
+++ relatorio/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import mysql.connector
+from config import config
+
 
 app = Flask(__name__)
+# exemplo para conectar no banco local:
+# config = {'user': 'seu usuario','password': 'senha',
+#           'host':'localhost','database':'nome do banco'}
+# Substituir e Descomentar as duas linhas acima
+# Comentar a linha 3: from config import config
+mydb = mysql.connector.connect(**config)
+teste = mydb.cursor()
 
-@app.route('index_relatorio')
+teste.execute("SELECT * FROM relatorio")
+
+resultado = teste.fetchall()
+
+@app.route('/index_relatorio')
 def index_rel():
-    return 'oi'
+    return f'{resultado}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
