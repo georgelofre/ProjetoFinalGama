5a25b12e2720add5934fc0aa153ec2e5943b6152
georgelofre <georgelofre@gmail.com>
2022-12-09 14:27:00 -0300

    criando a rota de mostrar produto mais vendido

--- relatorio/app.py
+++ relatorio/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import mysql.connector
 from config import config
 
@@ -11,14 +11,22 @@ app = Flask(__name__)
 # Comentar a linha 3: from config import config
 mydb = mysql.connector.connect(**config)
 teste = mydb.cursor()
+teste.execute("SELECT * FROM testevendas.relatorio")
+tudo = teste.fetchall()
+print(tudo)
 
-teste.execute("SELECT * FROM relatorio")
 
-resultado = teste.fetchall()
 
 @app.route('/index_relatorio')
 def index_rel():
-    return f'{resultado}'
+    return render_template('index_rel.html')
+
+@app.route('/index_relatorio/maisvendido')
+def maisvendido():
+    teste.execute("SELECT produto FROM testevendas.relatorio wEHERE")
+    maisvend = teste.fetchall()
+    print(maisvend)
+    return render_template('maisvendido.html', maisvendido=maisvend)
 
 if __name__ == '__main__':
     app.run(debug=True)
