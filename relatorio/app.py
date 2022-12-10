97d62fc8a12757ecc17a11da899602c769d6563c
georgelofre <georgelofre@gmail.com>
2022-12-10 02:22:46 -0300

    criando as rotas de produtos mais vendidos e de total de produtos com o valor

--- relatorio/app.py
+++ relatorio/app.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 import mysql.connector
 from config import config
 
 
 app = Flask(__name__)
+
 # exemplo para conectar no banco local:
 # config = {'user': 'seu usuario','password': 'senha',
 #           'host':'localhost','database':'nome do banco'}
 # Substituir e Descomentar as duas linhas acima
 # Comentar a linha 3: from config import config
+
+#conexão com o banco mysql
+#sugestão de preenchimento do banco: olhar arquivo tabeladb.txt
 mydb = mysql.connector.connect(**config)
 teste = mydb.cursor()
-teste.execute("SELECT * FROM testevendas.relatorio")
+teste.execute("SELECT * FROM testevendas.relatorio2")
 tudo = teste.fetchall()
 print(tudo)
-
-
 
 @app.route('/index_relatorio')
 def index_rel():
@@ -23,10 +25,34 @@ def index_rel():
 
 @app.route('/index_relatorio/maisvendido')
 def maisvendido():
-    teste.execute("SELECT produto FROM testevendas.relatorio wEHERE")
-    maisvend = teste.fetchall()
-    print(maisvend)
-    return render_template('maisvendido.html', maisvendido=maisvend)
+    teste.execute("select produto from relatorio where quantidade = (select max(quantidade) from relatorio);")
+    maisvend = teste.fetchone()
+    print(maisvend[0])
+    return render_template('maisvendido.html', maisvendido=maisvend[0])
+
+@app.route('/index_relatorio/osmaisvendidos')
+def osmaisvendidos():
+    teste.execute("select produto, quantidade from relatorio where quantidade > (select avg(quantidade) from relatorio);")
+    osmaisvend = teste.fetchall()
+    print(osmaisvend)
+    return render_template('osmaisvendidos.html', osmaisvendidos=osmaisvend)
+
+@app.route('/index_relatorio/totaldevendas')
+def totalDeVendas():
+    teste.execute("select sum(quantidade) from relatorio2;")
+    quant = teste.fetchone()
+    print(type(quant[0]))
+    print(quant)
+    quantidade = int(quant[0])
+    print(quantidade)
+    print(f'Qtde: {quantidade}')
+    teste.execute("select sum(valor) from relatorio2;")
+    arrec = teste.fetchone()
+    print(arrec)
+    arrecadado = f'{float(arrec[0]):.2f}'.replace(".", ",")
+    print(f'Qtde: {arrecadado}')
+    dados = [quantidade, arrecadado]
+    return render_template('totalvendas.html', dados=dados)
 
 if __name__ == '__main__':
     app.run(debug=True)
