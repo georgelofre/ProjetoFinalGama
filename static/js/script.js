7966fd908ba47f9b1c2f036494c1b587ce1fef10
Bruna Muñoz <64742826+Brunarnm@users.noreply.github.com>
2022-12-12 14:55:52 -0300

    Adição da pasta static

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+/*** Função para abrir e fechar o menu slider ***/
+function openMenu() {
+    var x = document.getElementById("slider");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } 
+        
+        else {
+            x.style.display = "none";
+        }
+}
